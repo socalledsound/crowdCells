437660008a7c02f0de897308f1d66ffd806c5c83
Douglas Fleischut <df@douglass-imac.lan>
2020-05-04 14:58:17 -0700

    refactor complete

---
+++ js/Cell.js
@@ -0,0 +1,181 @@
+function Cell(index){
+    this.index = index;
+    this.soundNum = this.index%totalNumSounds;
+    this.sound = new Howl({ src: `sounds/page1/${this.soundNum}.mp3`, html5: false, volume:0, loop: true});
+    this.sound.volume(0);
+    this.container = document.querySelector('.container');
+    this.container.style = getComputedStyle(this.container);
+    this.marginTop = 200;
+    this.initSize = 0;
+    this.size = this.initSize;
+    this.targetSize = Math.random()*(window.innerWidth/4);
+    this.sizeDifferential = this.targetSize - this.size;
+    this.xMin = window.innerWidth*.001;
+    this.yMin = window.innerWidth*.001 + 100;
+    this.safety = window.innerWidth*.1;
+    this.xMax = this.container.offsetWidth - this.safety - this.targetSize/2;
+    this.yMax = window.innerHeight - this.safety - 200;
+    this.x = Math.random() * this.xMax - this.size + this.xMin - this.safety;
+    this.y = Math.random() * this.yMax - this.yMin;
+    this.height = this.size;
+    this.width = this.size;
+    this.xSpeed = Math.random()*3;
+    this.ySpeed = Math.random()*-1;
+    this.impulse = this.targetSize/10;
+    this.rotate = 0;
+    this.rotateSpeed=Math.random()*5;
+    this.theta = 0;
+    this.oscillatingOpacity = 0.1;
+    this.img = 'white.jpg';
+    this.blurVal = Math.random()*10 + 2;
+    this.clicked = false;
+    this.mouseOver = false;
+    this.makingBigger = true;
+    this.playing = false;
+    this.completed = false;
+    
+    this.init=function(){
+        this.sound.fade(0.0, 0.05, 2000);
+        this.el=document.createElement('div');
+        this.el.className = "organism";
+        this.container = document.querySelector('.container');
+        this.sibling = document.querySelector('.child');
+        this.container.insertBefore(this.el, this.sibling)
+        this.addEventListeners();
+    }
+
+    this.addEventListeners = function(){
+        this.el.addEventListener("mouseout", (e)=>{
+            this.mouseOver = false;
+            this.sound.fade(0.3, 0.05, 1000);            
+        });
+
+        this.el.addEventListener("mouseenter", (e)=>{
+            this.mouseOver = true;
+            this.sound.volume(0.6);      
+       });
+
+        this.el.addEventListener("click", (e)=>{
+            this.coverScreen();
+        });
+    }
+
+    this.checkEdges = function(){
+        if(this.x > this.xMax + this.impulse + this.safety ){
+            this.x-=1;
+            this.xSpeed*=-1;
+            this.img = `${this.index%totalNumImages}.jpg`;
+            setTimeout(this.blur, 2000);
+            setTimeout(this.makeBigger, 100);
+
+            if(!this.playing){
+                this.playing = true;
+                this.sound.play();
+            }
+        }
+        if(this.x < 0 + this.xMin){
+            this.makeSmaller();   
+            this.xSpeed*=-1;
+            this.x+=5;
+            setTimeout(this.blur, 2000);
+            if(this.playing){
+                this.playing = false;
+                this.sound.stop();
+            }
+        }
+        
+        if(this.y + this.height/2 > this.yMax || this.y < 100){
+            this.ySpeed*=-1;
+        }
+    }
+
+
+    this.move= function(){   
+        if(!this.mouseOver){
+            this.x+=this.xSpeed;
+            this.y+=this.ySpeed;
+            this.theta += .01;
+            this.rotate+=this.rotateSpeed
+        }
+
+    }
+
+    this.display = function(){ 
+        this.oscillatingOpacity = Math.sin(this.theta)*0.25;
+        this.el.style.position='absolute';
+        this.el.style.height=`${this.height}px`;
+        this.el.style.width=`${this.width}px`;
+        this.el.style.left=`${this.x}px`;
+        this.el.style.top=`${this.y}px`;
+        this.el.style.transform=`rotate(${this.rotate}deg)`;
+        this.el.style.borderRadius="180px";
+        this.el.style.backgroundImage=`url('img/${this.img}')`
+        if(!this.mouseOver){
+            this.el.style.opacity = ".1";
+            this.blur(); 
+        } else {
+            this.el.style.opacity = ".6";
+            this.unBlur();
+        }
+    }
+
+    this.blur = function(){    
+        this.el.style.filter=`blur(${this.blurVal}px) brightness(80%)`;
+    }.bind(this)
+
+    this.unBlur = function(){    
+        this.el.style.filter="blur(0px) brightness(80%)";
+    }.bind(this)
+
+    this.makeBigger = function(){
+         
+        if(this.height < this.targetSize){
+            this.height+=5;
+            this.width+=5;
+            this.x-=this.impulse;
+            if(this.impulse>0){
+                this.impulse-=1
+            } 
+            
+            this.makeBigger();
+        }
+        
+        this.impulse=this.targetSize/6;
+
+    }.bind(this);
+
+    this.makeSmaller = function(){
+        this.height = this.initSize;
+        this.width = this.initSize;
+    }.bind(this);
+
+
+    this.coverScreen  = function(){
+        this.unBlur();
+        if(this.height > (this.targetSize * 2)){
+            this.blur;
+            this.el.style.zIndex = "-1";
+            this.completed = true;
+        }
+        if(this.height < this.yMax){
+            // this.el.style.zIndex = "-1";
+            this.height+=10;
+            this.width+=10;
+            this.y-=this.height/100;
+            this.x-=this.width/100;
+             setTimeout(this.coverScreen, 100);
+        } else {
+            this.showArrow();
+        }
+    }.bind(this)
+
+    this.showArrow = function(){
+        
+    }
+
+    // this.addSize = function(){
+    //     this.height+=this.sizeDifferential/10;
+    //     this.width+=this.sizeDifferential/10;
+    // }.bind(this);
+
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-let organisms = [];
-let totalOrganisms = 300;
-let organismCount = 0;
-let organismMultiplier  = 5;
+let cells = [];
+let totalcells = 300;
+let cellCount = 0;
+let cellMultiplier  = 5;
 let totalNumImages = 77;
 let totalNumSounds = 21;
 let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
@@ -20,7 +20,7 @@ function touchmove(e) {
 async function preload(){
     document.querySelector('.btn-start').remove();
     document.querySelector('h1').remove();
-    mainSound = new Howl({ src: `sounds/mainloop.mp3`, html5: false, volume:0.4, loop: true});
+    mainSound = new Howl({ src: `sounds/page1/mainloop.mp3`, html5: false, volume:0.4, loop: true});
     
     mainSound.once('load', function(){
         mainSound.play();
@@ -35,268 +35,49 @@ async function preload(){
 
 function init(){
 
-    let numOrganisms = Math.floor(Math.random()*10) + 3;
-    console.log(numOrganisms);
-    let stopLoop = organismCount + numOrganisms;
-    for(let i = organismCount; i < stopLoop; i++){
+    let numcells = Math.floor(Math.random()*10) + 3;
+    console.log(numcells);
+    let stopLoop = cellCount + numcells;
+    for(let i = cellCount; i < stopLoop; i++){
         
-        organisms[i] = new Organism(i);
-        organisms[i].init();
-        organisms[i].display();
-        organismCount+=1;
+        cells[i] = new Cellc(i);
+        cells[i].init();
+        cells[i].display();
+        cellCount+=1;
     }
 
 
     mainLoop();
 }
 
-function addOrganisms(){
-    let numOrganisms = Math.floor(Math.random() * organismMultiplier) + 3;
-    let stopLoop = organismCount + numOrganisms;
-    for(let i = organismCount; i < stopLoop; i++){
-        organisms[i] = new Organism(i);
-        organisms[i].init();
-        organisms[i].display();
-        organismCount++
+function addCells(){
+    let numcells = Math.floor(Math.random() * cellMultiplier) + 3;
+    let stopLoop = cellCount + numcells;
+    for(let i = cellCount; i < stopLoop; i++){
+        cells[i] = new Cellc(i);
+        cells[i].init();
+        cells[i].display();
+        cellCount++
     }
-    organismMultiplier+=2;
+    cellMultiplier+=2;
 }
 
 
 
 function mainLoop(){
-    console.log(organisms.length);
-    if(Math.random() > 0.99 && organismCount < totalOrganisms){
-        addOrganisms();
+    console.log(cells.length);
+    if(Math.random() > 0.99 && cellCount < totalcells){
+        addCells();
     }
-
-    organisms.forEach(organism=>organism.move());
-    organisms.forEach(organism=>organism.display());
+    cells.forEach(cell=>cell.checkEdges());
+    cells.forEach(cell=>cell.move());
+    cells.forEach(cell=>cell.display());
     window.requestAnimationFrame(mainLoop);
 }
 
-function Organism(index){
-    this.index = index;
-
-    this.soundNum = this.index%totalNumSounds;
-    this.sound = new Howl({ src: `sounds/${this.soundNum}.mp3`, html5: false, volume:0, loop: true});
-    this.container = document.querySelector('.container');
-    this.container.style = getComputedStyle(this.container);
-    console.log("margin" + this.container.style.content);
-    this.marginTop = 200;
-    this.initSize = 0;
-    this.targetSize = Math.random()*200;
-    this.sizeDifferential = this.targetSize - this.size;
-    this.size = this.initSize;
-    this.xMin = window.innerWidth*.001;
-    this.yMin = window.innerWidth*.001 + 100;
-    this.safety = window.innerWidth*.1;
-    this.xMax = this.container.offsetWidth - this.safety - this.targetSize/2;
-    // console.log("xMax:" + this.xMax);
-    this.yMax = window.innerHeight - this.safety - 200;
-     console.log("yMax:" + this.yMax);
-    this.x=Math.random() * this.xMax - this.size + this.xMin - this.safety;
-    // this.y=Math.random() * this.yMax - this.size + this.yMin - this.safety;
-    this.y = Math.random() * this.yMax + this.yMin;
-    this.height=this.size;
-    this.width=this.size;
-    this.xSpeed=Math.random()*3;
-    this.ySpeed=Math.random()*-1;
-    this.rotate = 0;
-    this.rotateSpeed=Math.random()*5;
-    // this.img=imageList[Math.floor(Math.random()*imageList.length)];
-    this.img='white.jpg';
-    this.theta = 0;
-    this.oscillatingOpacity = 0.1;
-    this.clicked = false;
-    this.mouseOver = false;
-    this.makingBigger=true;
-    this.playing = false;
-    this.sound.volume(0);
-    this.impulse=this.targetSize/6;
-    
-    this.init=function(){
-        this.sound.fade(0.0, 0.05, 2000);
-        this.el=document.createElement('div');
-        this.el.className = "organism";
-        this.el.addEventListener("mouseout", (e)=>{
-            this.mouseOver = false;
-            this.el.style.opacity = ".1";
-            this.el.style.filter="blur(3px) brightness(80%)";   
-            this.sound.fade(0.3, 0.05, 1000);
-            if(this.clicked){
-                this.sound.volume(0.3);
-            }
-            
-        });
-
-        this.el.addEventListener("mouseenter", (e)=>{
-            this.mouseOver = true;
-            this.el.style.opacity = ".6";
-            this.el.style.filter="blur(0px) brightness(100%)"; 
-            this.sound.volume(0.5);
-            if(this.clicked){
-                this.sound.volume(0.4);
-            }  
-       });
-
-        this.el.addEventListener("click", (e)=>{
-            this.coverScreen();
-            
-            
-            // if(!this.playing){
-            //     this.clicked = true;
-            //     // this.img=images[Math.floor(Math.random()*images.length)];
-            //     setTimeout(this.blur, 2000);
-            //     this.id1 = this.sound.stop();
-            // } else {
-                
-            //     this.clicked = false;
-            //     this.id1 = this.sound.play();
-            // }
-
-            // this.el.style.backgroundColor = "purple";
-            
-        });
-        this.container = document.querySelector('.container');
-        this.sibling = document.querySelector('.child');
-        // console.log(this.container);
-        this.container.insertBefore(this.el, this.sibling)
-
-    }
-
-
-    this.move= function(){
-
-        if(this.x > this.xMax + this.impulse + this.safety ){
-            // this.img=imageList[Math.floor(Math.random()*imageList.length)];
-            // this.img = "1.jpg";
-            this.x-=1;
-            this.xSpeed*=-1;
-            
-            setTimeout(this.blur, 2000);
-            this.img = `${this.index%totalNumImages}.jpg`
-            // this.img=`${Math.floor(Math.random()*this.totalNumImages)}.jpg`;
-            // this.img=`${Math.floor(Math.random()*this.totalNumImages)}.jpg`;
-            // this.el.style.filter="blur(0px) brightness(100%)"; 
-            setTimeout(this.makeBigger, 100);
-
-            if(!this.playing){
-                this.playing = true;
-                this.sound.play();
-            }
-        }
-        if(this.x < 0 + this.xMin){
-            this.makeSmaller();
-           
-            this.xSpeed*=-1;
-            this.x+=5;
-            
-            setTimeout(this.blur, 2000);
-            if(this.playing){
-                this.playing = false;
-                this.sound.stop();
-            
-            }
-        }
-        if(this.x < 0 + this.xMin){
-            
-            // this.xSpeed*=-1;
-            
-            // setTimeout(this.blur, 2000);
-            // if(this.clicked){
-                
-            // }
-        }
-        if(this.y + this.height/2 > this.yMax || this.y < 100){
-            this.ySpeed*=-1;
-        }
-        
-        if(!this.mouseOver){
-            this.x+=this.xSpeed;
-            this.y+=this.ySpeed;
-            this.rotate+=this.rotateSpeed
-        }
-;
-    }
-
-    this.display = function(){
-        this.theta += .01;
-        this.oscillatingOpacity = Math.sin(this.theta)*0.25;
-        this.el.style.position='absolute';
-        this.el.style.height=`${this.height}px`;
-        this.el.style.width=`${this.width}px`;
-        this.el.style.left=`${this.x}px`;
-        this.el.style.top=`${this.y}px`;
-        this.el.style.transform=`rotate(${this.rotate}deg)`;
-        this.el.style.borderRadius="180px";
-        this.el.style.backgroundImage=`url('img/${this.img}')`
-        if(!this.mouseOver){
-            this.el.style.opacity = ".1";
-        }
-        
-        //this.el.style.opacity = `${this.oscillatingOpacity}`;
-    }
-
-    this.blur = function(){
-        
-        let blurVal = Math.random()*10 + 2;
-        // this.el.style.opacity = `${this.oscillatingOpacity}`;
-        this.el.style.filter=`blur(${blurVal}px) brightness(80%)`;
-
-    }.bind(this)
-
-    this.makeBigger = function(){
-         
-        if(this.height < this.targetSize){
-            this.height+=5;
-            this.width+=5;
-            this.x-=this.impulse;
-            if(this.impulse>0){
-                this.impulse-=1
-            } 
-            
-            this.makeBigger();
-        };
-        
-        this.impulse=this.targetSize/6;
-
-    }.bind(this);
-
-    this.makeSmaller = function(){
-        this.height = this.initSize;
-        this.width = this.initSize;
-    }.bind(this);
-
-
-    this.coverScreen  = function(){
-        
-        if(this.height > this.targetSize*2){
-            this.blur;
-            this.el.style.zIndex="-1";
-        }
-        if(this.height < this.yMax){
-            this.el.style.zIndex="-1";
-            this.height+=10;
-            this.width+=10;
-            this.y-=this.height/100;
-            this.x-=this.width/100;
-             setTimeout(this.coverScreen, 100);
-        } else {
-            // startPartTwo();
-        }
-    }.bind(this)
-
-
-    // this.addSize = function(){
-    //     this.height+=this.sizeDifferential/10;
-    //     this.width+=this.sizeDifferential/10;
-    // }.bind(this);
-
-}
 
 
 function startPartTwo(){
-    organisms = [];
+    cells = [];
     document.body.backgroundImage = "url('img/white.jpg')";
 }
